b8acf1cff1dc85222516bd55984b611da2eca775
Aditya Borase <borse.aditya7@gmail.com>
2024-10-09 20:31:42 +0530

    update routes.py to render dummy content on the index page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username':'aditya'}
+    posts = [
+        {
+            'author' : {'username' : 'ashish'},
+            'body': 'i played mini militia!'
+        },
+        {
+            'author': {'username':'om'},
+            'body': 'founder, thedesigncompany'
+        },
+        {
+            'author': {'username':'shravani'},
+            'body': 'nothing, just trying the app'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)

b7e28e18e21241b7218441ea4a5f05c25f430883
Aditya Borase <borse.aditya7@gmail.com>
2024-10-20 22:55:23 +0530

    Implemented methods to generate and verify JWT-based password reset tokens.

--- app/models.py
+++ app/models.py
@@ -2,10 +2,12 @@ from typing import Optional
 from datetime import datetime, timezone
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db, login
+from app import db, login, app
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
+from time import time
+import jwt
 
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -81,6 +83,18 @@ class User(UserMixin, db.Model):
             .group_by(Post)
             .order_by(Post.timestamp.desc())
         )
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in}, app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return db.session.get(User, id)   
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
